2f383fedaa6a7d080e4a56f9be4111755a965f4a
areebatahi <areebatahirarain@gmail.com>
2025-02-06 01:26:19 +0500

    tailwind project

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Client from './assets/components/Client'
 import Economy from './assets/components/Economy'
 import Header from './assets/components/Header'
 import Navbar from './assets/components/Navbar'
+import Teacher from './assets/components/Teacher'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
      <Header />
      <Economy />
      <Client />
+     <Teacher />
     </>
   )
 }
---
+++ src/assets/components/Teacher.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+
+const Teacher = () => {
+    return (
+        <>
+            <div className="m-0 pt-0 h-fit grid grid-rows-1 flex-row overflow-hidden bg-gray-100">
+                <div className="mx-auto flex justify-center items-center flex-col">
+                    <p className='font-semibold'>Teacher's Panel</p>
+                    <h2 className="text-5xl font-bold text-black">See Our Talented Teacher</h2>
+                    <div className="flex justify-between items-center text-center">
+                        <div className="mr-33 mt-7">
+                            <img src="src\assets\img\images.jpeg" className="w-35 rounded-[100px]" />
+                            <p className='font-semibold text-xl'>Roy Hardy</p>
+                            <p className=''>Web Developer</p>
+                            <div className="pt-3 flex justify-center items-center">
+                                <img src="src\assets\img\emailIcon.jpg" className="w-6" />
+                                <img src="src\assets\img\twiter.png" className="w-5" />
+                                <img src="src\assets\img\linkend.PNG" className="w-6" />
+                                <img src="src\assets\img\git.png" className="w-5" />
+                            </div>
+                        </div>
+                        <div className="mr-33 mt-7">
+                            <img src="src\assets\img\images (1).jpg" className="w-35 rounded-[100px]" />
+                            <p className='font-semibold text-xl'>Jake Burke</p>
+                            <p className=''>Graphic Designer</p>
+                            <div className="pt-3 flex justify-center items-center">
+                                <img src="src\assets\img\emailIcon.jpg" className="w-6" />
+                                <img src="src\assets\img\twiter.png" className="w-5" />
+                                <img src="src\assets\img\linkend.PNG" className="w-6" />
+                                <img src="src\assets\img\git.png" className="w-5" />
+                            </div>
+                        </div>
+                        <div className="mr-33 mt-7">
+                            <img src="src\assets\img\images (2).jpeg" className="w-35 rounded-[100px]" />
+                            <p className='font-semibold text-xl'>Donnie Houston</p>
+                            <p className=''>Visual Designer</p>
+                            <div className="pt-3 flex justify-center items-center">
+                                <img src="src\assets\img\emailIcon.jpg" className="w-6" />
+                                <img src="src\assets\img\twiter.png" className="w-5" />
+                                <img src="src\assets\img\linkend.PNG" className="w-6" />
+                                <img src="src\assets\img\git.png" className="w-5" />
+                            </div>
+                        </div>
+                        <div className="mr-33 mt-7">
+                            <img src="src\assets\img\images (3).jpeg" className="w-35 rounded-[100px]" />
+                            <p className='font-semibold text-xl'>Vicki Morales</p>
+                            <p className=''>App Developer</p>
+                            <div className="pt-3 flex justify-center items-center">
+                                <img src="src\assets\img\emailIcon.jpg" className="w-6" />
+                                <img src="src\assets\img\twiter.png" className="w-5" />
+                                <img src="src\assets\img\linkend.PNG" className="w-6" />
+                                <img src="src\assets\img\git.png" className="w-5" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Teacher
