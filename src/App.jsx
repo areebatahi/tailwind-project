67d9ddb44d11063f629bac3a9b79ffa1ebd59261
areebatahi <areebatahirarain@gmail.com>
2025-06-19 19:37:21 +0500

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Client from './assets/components/Client'
-import ClientsSay from './assets/components/clientsSay'
+import ClientsSay from './assets/components/ClientsSay'
 import Courses from './assets/components/Courses'
 import Economy from './assets/components/Economy'
 import Footer from './assets/components/Footer'
@@ -8,18 +8,19 @@ import Header from './assets/components/Header'
 import Navbar from './assets/components/Navbar'
 import Teacher from './assets/components/Teacher'
 
+
 function App() {
 
   return (
     <>
-     <Navbar />
-     <Header />
-     <Economy />
-     <Client />
-     <Teacher />
-     <ClientsSay/>
-     <Courses />
-     <Footer />
+     <Navbar/>
+    <Header/>
+     <Economy/>
+     <Client/>
+     <Teacher/>
+    <ClientsSay/>
+     <Courses/>
+     <Footer/>
     </>
   )
 }
