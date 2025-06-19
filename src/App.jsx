3fbf4eca2e79ab10bd952d4b79e47573ed842f22
areebatahi <areebatahirarain@gmail.com>
2025-06-19 20:11:10 +0500

    component folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
-import Client from './components/Client'
-import ClientsSay from './components/ClientsSay'
-import Courses from './components/Courses'
-import Economy from './components/Economy'
-import Footer from './components/Footer'
-import Header from './components/Header'
-import Navbar from './components/Navbar'
-import Teacher from './components/Teacher'
+import Client from './assets/components/Client'
+import ClientsSay from './assets/components/ClientsSay'
+import Courses from './assets/components/Courses'
+import Economy from './assets/components/Economy'
+import Footer from './assets/components/Footer'
+import Header from './assets/components/Header'
+import Navbar from './assets/components/Navbar'
+import Teacher from './assets/components/Teacher'
 
 
 function App() {
