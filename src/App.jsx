ab6a028794040c9d78d8b4e20e57379bebf944aa
areebatahi <areebatahirarain@gmail.com>
2025-06-19 20:28:41 +0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
-import './App.css'
-import Client from './assets/components/Client'
+import "./App.css";
+import Client from "./assets/components/Client";
 import ClientsSay from "./assets/components/ClientsSay";
-import Courses from './assets/components/Courses'
-import Economy from './assets/components/Economy'
-import Footer from './assets/components/Footer'
-import Header from './assets/components/Header'
-import Navbar from './assets/components/Navbar'
-import Teacher from './assets/components/Teacher'
-
+import Courses from "./assets/components/Courses";
+import Economy from "./assets/components/Economy";
+import Footer from "./assets/components/Footer";
+import Header from "./assets/components/Header";
+import Navbar from "./assets/components/Navbar";
+import Teacher from "./assets/components/Teacher";
 
 function App() {
-
   return (
     <>
-     <Navbar/>
-    <Header/>
-     <Economy/>
-     <Client/>
-     <Teacher/>
-    <ClientsSay/>
-     <Courses/>
-     <Footer/>
+      <Navbar />
+      <Header />
+      <Economy />
+      <Client />
+      <Teacher />
+      <ClientsSay />
+      <Courses />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
